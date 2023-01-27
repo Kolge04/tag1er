a5150b8c2a31a1638d3b694dea3d1d85aea6d369
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-01-27 12:14:09 +0400

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -4,7 +4,7 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from pyrogram import filters, Client
+from pyrogram import filters
 from Aykhan.komekci.baza import aykhan2
 
 
