9256bf939b130ed0bcd9773778065bd3f8d95bb6
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-01-27 12:01:22 +0400

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -3,6 +3,10 @@ from telethon import Button
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
+import pyrogram
+from pyrogram import filters, Client
+from Aykhan.komekci.baza import aykhan2
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -66,6 +70,45 @@ async def handler(event):
 		    ),
                     link_preview=False)
                    
+	
+
+	
+	
+@aykhan2.on_message(
+    filters.command(['id'],prefixes=['/', '!'])
+    & (filters.group | filters.private)
+    & ~ filters.edited)
+async def get_ids(client, message):
+      if message.reply_to_message:
+         chat = message.reply_to_message.from_user.id
+         file_id = None
+         if message.reply_to_message.media:
+            if message.reply_to_message.audio:
+               file_id = message.reply_to_message.audio.file_id
+            elif message.reply_to_message.document:
+               file_id = message.reply_to_message.document.file_id
+            elif message.reply_to_message.photo:
+               file_id = message.reply_to_message.photo.file_id
+            elif message.reply_to_message.sticker:
+               file_id = message.reply_to_message.sticker.file_id
+            elif message.reply_to_message.voice:
+               file_id = message.reply_to_message.voice.file_id
+            elif message.reply_to_message.video_note:
+               file_id = message.reply_to_message.video_note.file_id
+            elif message.reply_to_message.video:
+               file_id=message.reply_to_message.video.id
+         if file_id is not None:
+             await message.reply("Qrup ID: `{}`\nİstifadəçi ID: `{}`\nFayl ID: `{}`".format(str(message.chat.id), str(chat), file_id))
+         else:
+             await message.reply("Qrup ID: `{}`\nİstifadəçi ID: `{}`".format(str(message.chat.id), str(chat)))
+      else:
+           await message.reply("Qrup ID: `{}`".format(str(message.chat.id)))
+            	
+	
+	
+	
+	
+	
 	
 	
 
